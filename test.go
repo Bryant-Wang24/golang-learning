3ae9db1e2e7ec670b00faec1cff789e3a5dc980e
Bryant-Wang24 <485969746@qq.com>
2021-11-03 21:09:25 +0800

    零值就是变量没有做初始化时系统默认设置的值。

--- test.go
+++ test.go
@@ -2,10 +2,27 @@
 package main
 import "fmt"
 // 可以一次声明多个变量：
-func main() {
-    var a string = "Runoob"
-    fmt.Println(a)
+// func main() {
+//     var a string = "Runoob"
+//     fmt.Println(a)
+//
+//     var b, c int = 1, 2
+//     fmt.Println(b, c)
+// }
 
-    var b, c int = 1, 2
-    fmt.Println(b, c)
+
+// 零值就是变量没有做初始化时系统默认设置的值。
+func main() {
+
+    // 声明一个变量并初始化
+    var a = "NOOB"
+    fmt.Println(a)//NOOB
+
+    // 没有初始化就为零值
+    var b int
+    fmt.Println(b)//0
+
+    // bool 零值为 false
+    var c bool
+    fmt.Println(c)//false
 }
