93190559ef26d5197a07109497d626eec2ae619d
王秋爽 <wangqiushuang@liankenet.com>
2021-11-04 16:53:57 +0800

    go语言运算符---关系运算符

--- test.go
+++ test.go
@@ -170,28 +170,28 @@ import "fmt"
 // 位运算符
 // 赋值运算符
 // 其他运算符
-func main() {
-
-   var a int = 21
-   var b int = 10
-   var c int
-
-   c = a + b
-   fmt.Printf("第一行 - c 的值为 %d\n", c )
-   c = a - b
-   fmt.Printf("第二行 - c 的值为 %d\n", c )
-   c = a * b
-   fmt.Printf("第三行 - c 的值为 %d\n", c )
-   c = a / b
-   fmt.Printf("第四行 - c 的值为 %d\n", c )
-   c = a % b
-   fmt.Printf("第五行 - c 的值为 %d\n", c )
-   a++
-   fmt.Printf("第六行 - a 的值为 %d\n", a )
-   a=21   // 为了方便测试，a 这里重新赋值为 21
-   a--
-   fmt.Printf("第七行 - a 的值为 %d\n", a )
-}
+// func main() {
+//
+//    var a int = 21
+//    var b int = 10
+//    var c int
+//
+//    c = a + b
+//    fmt.Printf("第一行 - c 的值为 %d\n", c )
+//    c = a - b
+//    fmt.Printf("第二行 - c 的值为 %d\n", c )
+//    c = a * b
+//    fmt.Printf("第三行 - c 的值为 %d\n", c )
+//    c = a / b
+//    fmt.Printf("第四行 - c 的值为 %d\n", c )
+//    c = a % b
+//    fmt.Printf("第五行 - c 的值为 %d\n", c )
+//    a++
+//    fmt.Printf("第六行 - a 的值为 %d\n", a )
+//    a=21   // 为了方便测试，a 这里重新赋值为 21
+//    a--
+//    fmt.Printf("第七行 - a 的值为 %d\n", a )
+// }
 // 运行结果
 // 第一行 - c 的值为 31
 // 第二行 - c 的值为 11
@@ -200,3 +200,41 @@ func main() {
 // 第五行 - c 的值为 1
 // 第六行 - a 的值为 22
 // 第七行 - a 的值为 20
+
+// 关系运算符
+func main() {
+   var a int = 21
+   var b int = 10
+
+   if( a == b ) {
+      fmt.Printf("第一行 - a 等于 b\n" )
+   } else {
+      fmt.Printf("第一行 - a 不等于 b\n" )
+   }
+   if ( a < b ) {
+      fmt.Printf("第二行 - a 小于 b\n" )
+   } else {
+      fmt.Printf("第二行 - a 不小于 b\n" )
+   }
+
+   if ( a > b ) {
+      fmt.Printf("第三行 - a 大于 b\n" )
+   } else {
+      fmt.Printf("第三行 - a 不大于 b\n" )
+   }
+   /* Lets change value of a and b */
+   a = 5
+   b = 20
+   if ( a <= b ) {
+      fmt.Printf("第四行 - a 小于等于 b\n" )
+   }
+   if ( b >= a ) {
+      fmt.Printf("第五行 - b 大于等于 a\n" )
+   }
+}
+// 运行结果
+// 第一行 - a 不等于 b
+// 第二行 - a 不小于 b
+// 第三行 - a 大于 b
+// 第四行 - a 小于等于 b
+// 第五行 - b 大于等于 a
