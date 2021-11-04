2d12f0eb1df121cdabc404bec0463a07de9a3e1d
王秋爽 <wangqiushuang@liankenet.com>
2021-11-04 16:56:40 +0800

    go语言运算符---逻辑运算符

--- test.go
+++ test.go
@@ -202,39 +202,67 @@ import "fmt"
 // 第七行 - a 的值为 20
 
 // 关系运算符
-func main() {
-   var a int = 21
-   var b int = 10
-
-   if( a == b ) {
-      fmt.Printf("第一行 - a 等于 b\n" )
-   } else {
-      fmt.Printf("第一行 - a 不等于 b\n" )
-   }
-   if ( a < b ) {
-      fmt.Printf("第二行 - a 小于 b\n" )
-   } else {
-      fmt.Printf("第二行 - a 不小于 b\n" )
-   }
-
-   if ( a > b ) {
-      fmt.Printf("第三行 - a 大于 b\n" )
-   } else {
-      fmt.Printf("第三行 - a 不大于 b\n" )
-   }
-   /* Lets change value of a and b */
-   a = 5
-   b = 20
-   if ( a <= b ) {
-      fmt.Printf("第四行 - a 小于等于 b\n" )
-   }
-   if ( b >= a ) {
-      fmt.Printf("第五行 - b 大于等于 a\n" )
-   }
-}
+// func main() {
+//    var a int = 21
+//    var b int = 10
+//
+//    if( a == b ) {
+//       fmt.Printf("第一行 - a 等于 b\n" )
+//    } else {
+//       fmt.Printf("第一行 - a 不等于 b\n" )
+//    }
+//    if ( a < b ) {
+//       fmt.Printf("第二行 - a 小于 b\n" )
+//    } else {
+//       fmt.Printf("第二行 - a 不小于 b\n" )
+//    }
+//
+//    if ( a > b ) {
+//       fmt.Printf("第三行 - a 大于 b\n" )
+//    } else {
+//       fmt.Printf("第三行 - a 不大于 b\n" )
+//    }
+//    /* Lets change value of a and b */
+//    a = 5
+//    b = 20
+//    if ( a <= b ) {
+//       fmt.Printf("第四行 - a 小于等于 b\n" )
+//    }
+//    if ( b >= a ) {
+//       fmt.Printf("第五行 - b 大于等于 a\n" )
+//    }
+// }
 // 运行结果
 // 第一行 - a 不等于 b
 // 第二行 - a 不小于 b
 // 第三行 - a 大于 b
 // 第四行 - a 小于等于 b
 // 第五行 - b 大于等于 a
+
+
+// 逻辑运算符
+func main() {
+   var a bool = true
+   var b bool = false
+   if ( a && b ) {
+      fmt.Printf("第一行 - 条件为 true\n" )
+   }
+   if ( a || b ) {
+      fmt.Printf("第二行 - 条件为 true\n" )
+   }
+   /* 修改 a 和 b 的值 */
+   a = false
+   b = true
+   if ( a && b ) {
+      fmt.Printf("第三行 - 条件为 true\n" )
+   } else {
+      fmt.Printf("第三行 - 条件为 false\n" )
+   }
+   if ( !(a && b) ) {
+      fmt.Printf("第四行 - 条件为 true\n" )
+   }
+}
+// 运行结果
+// 第二行 - 条件为 true
+// 第三行 - 条件为 false
+// 第四行 - 条件为 true
