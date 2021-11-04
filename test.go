d0fca4f1feff97f0511469515607253113e09738
王秋爽 <wangqiushuang@liankenet.com>
2021-11-04 16:24:03 +0800

    go语言常量（const）

--- test.go
+++ test.go
@@ -1,6 +1,7 @@
 
 package main
 // import "fmt"
+import "unsafe"
 // 可以一次声明多个变量：
 // func main() {
 //     var a string = "Runoob"
@@ -63,20 +64,50 @@ package main
 //     fmt.Println(f)//Noob
 // }
 
-var x, y int
-var (  // 这种因式分解关键字的写法一般用于声明全局变量
-    a int
-    b bool
+// var x, y int
+// var (  // 这种因式分解关键字的写法一般用于声明全局变量
+//     a int
+//     b bool
+// )
+//
+// var c, d int = 1, 2
+// var e, f = 123, "hello"
+//
+// //这种不带声明格式的只能在函数体中出现
+// //g, h := 123, "hello"
+//
+// func main(){
+//     g, h := 123, "hello"
+//     println(x, y, a, b, c, d, e, f, g, h)
+// }
+// 输出0 0 0 false 1 2 123 hello 123 hello
+
+// 常量是一个简单值的标识符，在程序运行时，不会被修改的量。(const)
+// func main() {
+//    const LENGTH int = 10
+//    const WIDTH int = 5
+//    var area int
+//    const a, b, c = 1, false, "str" //多重赋值
+//
+//    area = LENGTH * WIDTH
+//    fmt.Printf("面积为 : %d", area)
+//    println()
+//    println(a, b, c)
+// }
+// 运行结果：
+// 面积为 : 50
+// 1 false str
+
+
+// 常量还可以用作枚举：
+// 常量可以用len(), cap(), unsafe.Sizeof()函数计算表达式的值。常量表达式中，函数必须是内置函数，否则编译不过：
+const (
+    a = "abc"
+    b = len(a)
+    c = unsafe.Sizeof(a)
 )
 
-var c, d int = 1, 2
-var e, f = 123, "hello"
-
-//这种不带声明格式的只能在函数体中出现
-//g, h := 123, "hello"
-
 func main(){
-    g, h := 123, "hello"
-    println(x, y, a, b, c, d, e, f, g, h)
+    println(a, b, c)
 }
-// 输出0 0 0 false 1 2 123 hello 123 hello
+// abc 3 16
