faa67357cf961231bf903458ed81904defc3ec11
Bryant-Wang24 <485969746@qq.com>
2021-11-11 08:55:31 +0800

    go语言函数使用

--- test.go
+++ test.go
@@ -769,23 +769,51 @@ import "fmt"
 // Go 语言的 goto 语句可以无条件地转移到过程中指定的行。
 // goto 语句通常与条件语句配合使用。可用来实现条件转移， 构成循环，跳出循环体等功能。
 // 但是，在结构化程序设计中一般不主张使用 goto 语句， 以免造成程序流程的混乱，使理解和调试程序都产生困难。
-func main() {
-    /* 定义局部变量 */
-    var a int = 10
- 
-    /* 循环 */
-    LOOP: for a < 15 {
-       if a == 12 {
-          /* 跳过迭代 */
-          a = a + 1
-          goto LOOP
-       }
-       fmt.Printf("a的值为 : %d\n", a)
-       a++    
-    }  
- }
+// func main() {
+//     /* 定义局部变量 */
+//     var a int = 10
+
+//     /* 循环 */
+//     LOOP: for a < 15 {
+//        if a == 12 {
+//           /* 跳过迭代 */
+//           a = a + 1
+//           goto LOOP
+//        }
+//        fmt.Printf("a的值为 : %d\n", a)
+//        a++
+//     }
+//  }
 // 运行结果：
 // a的值为 : 10
 // a的值为 : 11
 // a的值为 : 13
 // a的值为 : 14
+
+// go语言的函数
+func main() {
+    /* 定义局部变量 */
+    var a int = 100
+    var b int = 200
+    var ret int
+ 
+    /* 调用函数并返回最大值 */
+    ret = max(a, b)
+ 
+    fmt.Printf( "最大值是 : %d\n", ret )
+ }
+ 
+ /* 函数返回两个数的最大值 */
+ func max(num1, num2 int) int {
+    /* 定义局部变量 */
+    var result int
+ 
+    if (num1 > num2) {
+       result = num1
+    } else {
+       result = num2
+    }
+    return result
+ }
+//  以上实例在 main() 函数中调用 max（）函数，执行结果为：
+// 最大值是 : 200
