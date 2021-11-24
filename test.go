ace1b05b57116630377021cc63a777e7efab4c65
Bryant-Wang24 <485969746@qq.com>
2021-11-24 09:11:29 +0800

    结构体之间的转换：强制转换

--- test.go
+++ test.go
@@ -1649,17 +1649,31 @@ type Player struct{
 
 
 // 结构体之间的转换
-// 结构体是用户单独定义的类型，和其他类型进行转换时需要有完全相同的字段（名字、个数和类型）
+// 1、结构体是用户单独定义的类型，和其他类型进行转换时需要有完全相同的字段（名字、个数和类型）
+// type Student struct{
+//     Age int
+// }
+// type Person struct{
+//     Age int
+// }
+// func main()  {
+//     var s Student = Student{10}
+//     var p Person = Person{10}
+//     s = Student(p)
+//     fmt.Println(s) //{10}
+//     fmt.Println(p) //{10}
+// }
+
+// 结构体之间的转换
+// 2、结构体进行type重新定义（相当于取别名），go认为是新的数据类型，但是互相间可以强转
 type Student struct{
     Age int
 }
-type Person struct{
-    Age int
-}
+type Stu Student
 func main()  {
-    var s Student = Student{10}
-    var p Person = Person{10}
-    s = Student(p)
-    fmt.Println(s) //{10}
-    fmt.Println(p) //{10}
+    var s1 Student = Student{19}
+    var s2 Stu = Stu{19}
+    s1 = Student(s2)
+    fmt.Println(s1) // {19}
+    fmt.Println(s2) // {19}
 }
